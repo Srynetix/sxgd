15a1ee7c456b5112959dd657ff314b93745f14d9
Denis BOURGE <denis.bourge@sharingcloud.com>
2022-06-12 17:50:01 +0200

    feat: tilemap dump/restore

--- docs/extensions/SxTileMap.md
+++ docs/extensions/SxTileMap.md
@@ -18,3 +18,102 @@
 >   
 > Example:  
 >   var r = SxTileMap.get_cell_rotation(tilemap, pos)  
+### `get_cell_rotation_params`
+
+*Prototype*: `static func get_cell_rotation_params(tilemap: TileMap, pos: Vector2) -> CellRotationParams`
+
+> Get rotation for a specific cell, in a custom class format.  
+### `rotation_params_to_angle`
+
+*Prototype*: `static func rotation_params_to_angle(params: CellRotationParams) -> float`
+
+> Convert rotation params to an angle in radians.  
+### `rotation_degrees_to_params`
+
+*Prototype*: `static func rotation_degrees_to_params(angle_degrees: int) -> CellRotationParams`
+
+> Generate cell rotation parameters from an angle in degrees.  
+### `create_dump`
+
+*Prototype*: `static func create_dump(tilemap: TileMap) -> Array`
+
+> Create a dump from a tilemap.  
+### `load_dump`
+
+*Prototype*: `static func load_dump(tilemap: TileMap, array: Array) -> void`
+
+> Apply a dump in a tilemap.  
+## CellRotationParams
+
+|    |     |
+|----|-----|
+|*Inherits from*|`Node`|
+
+> Helper class representing transpose/flip parameters for a TileMap cell  
+### CellRotationParams, Public variables
+
+#### `transpose`
+
+*Code*: `var transpose: bool`
+
+#### `flip_x`
+
+*Code*: `var flip_x: bool`
+
+#### `flip_y`
+
+*Code*: `var flip_y: bool`
+
+### CellRotationParams, Static methods
+
+#### `from_values`
+
+*Prototype*: `static func from_values(transpose: bool, flip_x: bool, flip_y: bool) -> CellRotationParams`
+
+> Create params instance from arguments  
+#### `from_array`
+
+*Prototype*: `static func from_array(array: Array) -> CellRotationParams`
+
+### CellRotationParams, Methods
+
+#### `to_array`
+
+*Prototype*: `func to_array() -> Array`
+
+## CellDump
+
+|    |     |
+|----|-----|
+|*Inherits from*|`Node`|
+
+### CellDump, Public variables
+
+#### `tile_id`
+
+*Code*: `var tile_id: int`
+
+#### `tile_pos`
+
+*Code*: `var tile_pos: Vector2`
+
+#### `rotation_params`
+
+*Code*: `var rotation_params: CellRotationParams`
+
+### CellDump, Static methods
+
+#### `from_values`
+
+*Prototype*: `static func from_values(tile_id: int, tile_pos: Vector2, rotation_params: CellRotationParams) -> CellDump`
+
+#### `from_array`
+
+*Prototype*: `static func from_array(array: Array) -> CellDump`
+
+### CellDump, Methods
+
+#### `to_array`
+
+*Prototype*: `func to_array() -> Array`
+
--- extensions/SxTileMap.gd
+++ extensions/SxTileMap.gd
@@ -1,34 +1,115 @@
 extends Reference
 class_name SxTileMap
 
+# Helper class representing transpose/flip parameters for a TileMap cell
+class CellRotationParams:
+    extends Node
+
+    var transpose: bool
+    var flip_x: bool
+    var flip_y: bool
+
+    # Create params instance from arguments
+    static func from_values(transpose: bool, flip_x: bool, flip_y: bool) -> CellRotationParams:
+        var obj = CellRotationParams.new()
+        obj.transpose = transpose
+        obj.flip_x = flip_x
+        obj.flip_y = flip_y
+        return obj
+
+    static func from_array(array: Array) -> CellRotationParams:
+        return CellRotationParams.from_values(array[0], array[1], array[2])
+
+    func to_array() -> Array:
+        return [transpose, flip_x, flip_y]
+
+class CellDump:
+    extends Node
+
+    var tile_id: int
+    var tile_pos: Vector2
+    var rotation_params: CellRotationParams
+
+    static func from_values(tile_id: int, tile_pos: Vector2, rotation_params: CellRotationParams) -> CellDump:
+        var obj = CellDump.new()
+        obj.tile_id = tile_id
+        obj.tile_pos = tile_pos
+        obj.rotation_params = rotation_params
+        return obj
+
+    func to_array() -> Array:
+        return [tile_id, [tile_pos.x, tile_pos.y], rotation_params.to_array()]
+
+    static func from_array(array: Array) -> CellDump:
+        return CellDump.from_values(array[0], Vector2(array[1][0], array[1][1]), CellRotationParams.from_array(array[2]))
+
 # Get rotation for a specific cell, in radians.
 #
 # Example:
 #   var r = SxTileMap.get_cell_rotation(tilemap, pos)
 static func get_cell_rotation(tilemap: TileMap, pos: Vector2) -> float:
-    var logger := SxLog.get_logger("SxTileMap")
+    var params = get_cell_rotation_params(tilemap, pos)
+    return rotation_params_to_angle(params)
 
+# Get rotation for a specific cell, in a custom class format.
+static func get_cell_rotation_params(tilemap: TileMap, pos: Vector2) -> CellRotationParams:
     var x = int(pos.x)
     var y = int(pos.y)
     var transposed = tilemap.is_cell_transposed(x, y)
     var flip_x = tilemap.is_cell_x_flipped(x, y)
     var flip_y = tilemap.is_cell_y_flipped(x, y)
 
-    if !transposed && !flip_x && !flip_y:
+    return CellRotationParams.from_values(transposed, flip_x, flip_y)
+
+# Convert rotation params to an angle in radians.
+static func rotation_params_to_angle(params: CellRotationParams) -> float:
+    var logger := SxLog.get_logger("SxTileMap")
+
+    if !params.transpose && !params.flip_x && !params.flip_y:
         return 0.0
 
-    if transposed && !flip_x && flip_y:
+    if params.transpose && !params.flip_x && params.flip_y:
         return -PI / 2
 
-    if !transposed && flip_x:
+    if !params.transpose && params.flip_x:
         return PI
 
-    if transposed && flip_x && !flip_y:
+    if params.transpose && params.flip_x && !params.flip_y:
         return PI / 2
 
     logger.warn(
-        "Unknown rotation for map %s, position %s (t: %s, fx: %s, fy: %s)"
-        % [tilemap, pos, transposed, flip_x, flip_y]
+        "Unknown rotation for params (t: %s, fx: %s, fy: %s)"
+        % [params.transpose, params.flip_x, params.flip_y]
     )
 
     return 0.0
+
+# Generate cell rotation parameters from an angle in degrees.
+static func rotation_degrees_to_params(angle_degrees: int) -> CellRotationParams:
+    assert(angle_degrees in [0, 90, 180, 270], "Unsupported angle %d in 'rotation_degrees_to_params'" % angle_degrees)
+
+    if angle_degrees == 90:
+        return CellRotationParams.from_values(true, true, false)
+    elif angle_degrees == 180:
+        return CellRotationParams.from_values(false, true, true)
+    elif angle_degrees == 270:
+        return CellRotationParams.from_values(true, false, true)
+    else:
+        return CellRotationParams.from_values(false, false, false)
+
+# Create a dump from a tilemap.
+static func create_dump(tilemap: TileMap) -> Array:
+    var array = []
+    for tile_pos in tilemap.get_used_cells():
+        var tile_idx = tilemap.get_cellv(tile_pos)
+        var params = get_cell_rotation_params(tilemap, tile_pos)
+        array.append(CellDump.from_values(tile_idx, tile_pos, params).to_array())
+    return array
+
+# Apply a dump in a tilemap.
+static func load_dump(tilemap: TileMap, array: Array) -> void:
+    tilemap.clear()
+
+    for item in array:
+        var dump = CellDump.from_array(item)
+        tilemap.set_cellv(dump.tile_pos, dump.tile_id, dump.rotation_params.flip_x, dump.rotation_params.flip_y, dump.rotation_params.transpose)
