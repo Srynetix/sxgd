ba7ff1051371911d9f2c00627cf08293cce61e6c
Denis BOURGE <bourge.denis@gmail.com>
2023-05-27 15:05:13 +0200

    fix: better blur

--- nodes/fx/screen-effects/SxFxBetterBlur/SxFxBetterBlur.gd
+++ nodes/fx/screen-effects/SxFxBetterBlur/SxFxBetterBlur.gd
@@ -3,14 +3,14 @@
 extends Control
 class_name SxFxBetterBlur
 
-const shader_x := preload("res://addons/sxgd/nodes/fx/screen-effects/SxFxBetterBlur/SxFxBetterBlurX.gdshader")
-const shader_y := preload("res://addons/sxgd/nodes/fx/screen-effects/SxFxBetterBlur/SxFxBetterBlurY.gdshader")
+const shader := preload("res://addons/sxgd/nodes/fx/screen-effects/SxFxBetterBlur/Shader.gdshader")
 
 @export var strength := 0.0 : set = _set_strength
 
 var _step1: ColorRect
 var _step2: ColorRect
-var _copy: BackBufferCopy
+var _step1_layer: CanvasLayer
+var _step2_layer: CanvasLayer
 
 func _set_strength(value: float) -> void:
     strength = value
@@ -26,25 +26,28 @@ func _ready() -> void:
     mouse_filter = Control.MOUSE_FILTER_IGNORE
 
     var material_x := ShaderMaterial.new()
-    material_x.shader = shader_x
+    material_x.shader = shader
+    material_x.set_shader_parameter("direction", Vector2(1.0, 0.0))
     var material_y := ShaderMaterial.new()
-    material_y.shader = shader_y
+    material_y.shader = shader
+    material_y.set_shader_parameter("direction", Vector2(0.0, 1.0))
+
+    _step1_layer = CanvasLayer.new()
+    add_child(_step1_layer)
+
+    _step2_layer = CanvasLayer.new()
+    add_child(_step2_layer)
 
     _step1 = ColorRect.new()
     _step1.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
     _step1.mouse_filter = Control.MOUSE_FILTER_IGNORE
     _step1.material = material_x
-    add_child(_step1)
-
-    _copy = BackBufferCopy.new()
-    _copy.copy_mode = BackBufferCopy.COPY_MODE_VIEWPORT
-    add_child(_copy)
+    _step1_layer.add_child(_step1)
 
     _step2 = ColorRect.new()
     _step2.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
     _step2.mouse_filter = Control.MOUSE_FILTER_IGNORE
     _step2.material = material_y
-    _copy.add_child(_step2)
+    _step2_layer.add_child(_step2)
 
-    _copy.rect = Rect2(position, size)
     _set_strength(strength)
